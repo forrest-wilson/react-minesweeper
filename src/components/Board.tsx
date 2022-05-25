ebac153c0375a2fa186564a320bc21d6e4beb54f
Forrest Wilson-Jennings <forrest.wilson@me.com>
2022-05-25 15:29:35 +1200

    Moderately working board

---
+++ src/components/Board.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { generateBoard, TCell } from "../helpers/board-generator";
+import './Board.css'
+
+function Board() {
+  const numRows = 10
+  const numCols = 10
+  const numBombs = 10
+
+  const [board, setBoard] = useState(generateBoard(numRows, numCols, numBombs))
+
+  const handleClick = (cell: TCell) => {
+    const copy = [...board]
+
+    let found = copy[cell.row][cell.column]
+    found.isShown = true
+
+    copy[cell.row].splice(cell.column, 1, found)
+
+    setBoard(copy)   
+  }
+
+  return (
+    <div className="board">
+      {board.map((row, i) => {
+        return <div className="row" key={i}>
+          {row.map(cell => <div key={cell.id} className={`cell ${cell.isShown ? 'is-shown' : ''}`} onClick={() => handleClick(cell)}>
+            <div className="text">{cell.row}, {cell.column}</div>
+          </div>)}
+        </div>
+      })}
+    </div>
+  );
+}
+
+export default Board;
---
+++ src/helpers/board-generator.ts
@@ -0,0 +1,49 @@
+import { v4 } from "uuid"
+
+export type TCell = {
+  id: string
+  isShown: boolean
+  hasBomb: boolean
+  row: number
+  column: number
+}
+
+export const generateBoard = (rows: number = 5, columns: number = 5, bombs: number = 5): TCell[][]  => {
+  if (bombs >= (rows * columns)) {
+    throw new Error("Number of bombs cannot exceed number of cells (rows x columns)")
+  }
+
+  const cells: TCell[] = [];
+  const boardLength = rows * columns
+
+  const bombIndexes: number[] = [];
+
+  // Pick random indices for the bombs, making sure that indices are unique
+  while (bombIndexes.length < bombs) {
+    let randomIndex = Math.floor(Math.random() * (boardLength - 1))
+
+    if (!bombIndexes.includes(randomIndex)) {
+      bombIndexes.push(randomIndex)
+    }
+  }
+
+  // Create the cells and push them to the cells array
+  // making sure to mark the indices with bombs
+  // and calculate the rows and columns for later use
+  for (let i = 0; i < boardLength; i++) {
+    // Rows and columns are 0 indexed
+    const cell: TCell = {
+      id: v4(),
+      isShown: false,
+      hasBomb: bombIndexes.includes(i),
+      row: Math.floor(i / columns),
+      column: i % columns
+    }
+
+    cells.push(cell)
+  }
+
+  return [...Array(rows)].map((_, i) => {
+    return cells.filter(val => val.row === i)
+  })
+}
